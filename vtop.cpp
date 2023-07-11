d3d4cbefa2e60c4228ec786b30e262f693a7fc0e
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-07-11 11:39:09 -0400

    Update vtop.cpp

--- vtop.cpp
+++ vtop.cpp
@@ -208,7 +208,7 @@ static void *thread_fn1(void *data)
 		
 		std::cout << "myvector stores " << int(task_stack.size()) << " numbers.\n";
 		
-		for (i = 0; i < LAST_CPU_ID; i++) {
+		for (int i = 0; i < LAST_CPU_ID; i++) {
 			std::cout  << " " << active_cpu_bitmap[i] << " ";
 		}
 		std::cout << "\n";
