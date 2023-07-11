f37f556ac68a655f5784a8f7061929030e82051e
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-07-11 13:23:44 -0400

    Update vtop.cpp

--- vtop.cpp
+++ vtop.cpp
@@ -26,7 +26,7 @@
 #define GROUP_NONLOCAL	(1)
 #define GROUP_GLOBAL	(2)
 
-#define NR_SAMPLES      (10)
+#define NR_SAMPLES      (50)
 #define SAMPLE_US       (10000)
 
 #define min(a,b)	(a < b ? a : b)
