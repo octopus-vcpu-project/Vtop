31e699cd060a1e28b4e63b37a77597cac10b6b2f
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-07-19 13:18:04 -0400

    Update vtop.cpp

--- vtop.cpp
+++ vtop.cpp
@@ -379,7 +379,7 @@ void apply_optimization_recur(int cpu, int last_cpu,int latency_class,std::unord
 	tested_arr[cpu] = 1;
 	int sub_rel = top_stack[cpu][last_cpu];
 	for(int x=0;x<LAST_CPU_ID;x++){
-		if(top_stack[cpu][x] < latency_class && (top_stack[cpu][x] != 0 && tested_arr[x] == 0)){
+		if(top_stack[cpu][x] < latency_class && (top_stack[cpu][x] != 0 && tested_arr[x] != 1)){
 			apply_optimization_recur(x,cpu,latency_class,tested_arr);
 		}
 		if(top_stack[last_cpu][x]== latency_class && top_stack[cpu][x]==0){
