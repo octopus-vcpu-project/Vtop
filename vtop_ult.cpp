df59fb963a68f34cf3cb573c8f79baeb4c676765
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-07-26 15:56:57 -0400

    Update vtop_ult.cpp

--- vtop_ult.cpp
+++ vtop_ult.cpp
@@ -343,9 +343,8 @@ int measure_latency_pair(int i, int j)
 	int test = 0;
 	for (test = 0; test < NR_SAMPLES; test++) {
 		usleep(SAMPLE_US);
-		atomic_t s;
+		atomic_t s = nr_pingpongs.x;
 		quick_test = 1; 
-		atomic_t s = __sync_lock_test_and_set(&nr_pingpongs.x, 0);
 		uint64_t time_stamp = now_nsec();
 		double sample = (time_stamp - last_stamp) / (double)s;
 		last_stamp = time_stamp;
