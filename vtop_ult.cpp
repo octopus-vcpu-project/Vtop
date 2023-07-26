911aaeb7c3388c17b6cefa865143e5bd8066260a
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-07-26 17:47:22 -0400

    Update vtop_ult.cpp

--- vtop_ult.cpp
+++ vtop_ult.cpp
@@ -366,6 +366,12 @@ int measure_latency_pair(int i, int j)
 	stop_loops = 0;
 	odd.buddy = 0;
 	pingpong_mutex = NULL;
+	for(int z=0;z<even.timestamps.size() - 1;z++){
+		double sample = (even.timestamps[z+1] - even.timestamps[z]) / (double)10000;
+		if ((sample < best_sample && sample != 1.0/0.)||(best_sample==1.0/0.)){
+			best_sample = sample;
+		}
+	}
 	std::cout << "I:"<<i<<" J:"<<j<<" Sample passed " << (int)(best_sample*100) << " next.\n";
 	return (int)(best_sample*100);
 }
