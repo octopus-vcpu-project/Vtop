4b686535a1d3c018c7aa9b38d2f9f8c634ddd64e
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-08-01 00:20:36 -0400

    Update vtop_ult.cpp

--- vtop_ult.cpp
+++ vtop_ult.cpp
@@ -926,7 +926,6 @@ int main(int argc, char *argv[])
 		//performProbing();
 		//popul_laten_now = now_nsec();
 		//printf("This time it took to find all topology%lf\n", (popul_laten_now-popul_laten_last)/(double)1000000);
-		stop_user_processes();
 		if(verbose){
 			print_population_matrix();
 		}
@@ -946,7 +945,6 @@ int main(int argc, char *argv[])
 			printf("TOPOLOGY FAILED.TOOK (MILLISECONDS):%lf\n", (popul_laten_now-popul_laten_last)/(double)1000000);
 			
 		}
-		resume_stopped_processes();
 		//popul_laten_now = now_nsec();
 		//printf("This time it took to verify%lf\n", (popul_laten_now-popul_laten_last)/(double)1000000);
 		//configure_os_numa_groups(1);
