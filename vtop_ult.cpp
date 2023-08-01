58a3acf47e34a57470ca1ff691eeb57accaf3450
FarWrong <55039915+FarWrong@users.noreply.github.com>
2023-08-01 00:07:08 -0400

    Update vtop_ult.cpp

--- vtop_ult.cpp
+++ vtop_ult.cpp
@@ -394,9 +394,6 @@ int measure_latency_pair(int i, int j)
 		usleep(100);
 	}
 	
-	if(amount_of_times>0){
-		SAMPLE_US = 8000;
-	}
 	std::cout<<'\n'<<"Sample time:"<<SAMPLE_US<<'\n'<<std::endl;
 	usleep(SAMPLE_US);
 	stop_loops = 1;
