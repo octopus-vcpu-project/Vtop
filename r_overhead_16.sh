cd3cdcf52241e44280c1349ba4993b0a8c4ece0d
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-08-09 16:41:32 -0400

    Update r_overhead_16.sh

--- r_overhead_16.sh
+++ r_overhead_16.sh
@@ -2,39 +2,39 @@ VM_NAME="e-vm1"
 COMPETITOR_VM="e-vm2"
 VTOP_CMD="./vtop/a.out -u 300000 -d 600 -s 5 -f 5"
 
-virsh vcpupin $VM_NAME 0 40
-virsh vcpupin $VM_NAME 1 41
-virsh vcpupin $VM_NAME 2 42
-virsh vcpupin $VM_NAME 3 43
-virsh vcpupin $VM_NAME 4 61
-virsh vcpupin $VM_NAME 5 62
-virsh vcpupin $VM_NAME 6 63
-virsh vcpupin $VM_NAME 7 64
-virsh vcpupin $VM_NAME 8 141
-virsh vcpupin $VM_NAME 9 142
-virsh vcpupin $VM_NAME 10 143
-virsh vcpupin $VM_NAME 11 144
-virsh vcpupin $VM_NAME 12 121
-virsh vcpupin $VM_NAME 13 122
-virsh vcpupin $VM_NAME 14 123
-virsh vcpupin $VM_NAME 15 124
+virsh vcpupin $VM_NAME 0 0
+virsh vcpupin $VM_NAME 1 0
+virsh vcpupin $VM_NAME 2 1
+virsh vcpupin $VM_NAME 3 80
+virsh vcpupin $VM_NAME 4 80
+virsh vcpupin $VM_NAME 5 2
+virsh vcpupin $VM_NAME 6 81
+virsh vcpupin $VM_NAME 7 3
+virsh vcpupin $VM_NAME 8 83
+virsh vcpupin $VM_NAME 9 83
+virsh vcpupin $VM_NAME 10 4
+virsh vcpupin $VM_NAME 11 84
+virsh vcpupin $VM_NAME 12 5
+virsh vcpupin $VM_NAME 13 85
+virsh vcpupin $VM_NAME 14 6
+virsh vcpupin $VM_NAME 15 86
 
-virsh vcpupin $COMPETITOR_VM 0 40
-virsh vcpupin $COMPETITOR_VM 1 41
-virsh vcpupin $COMPETITOR_VM 2 42
-virsh vcpupin $COMPETITOR_VM 3 43
-virsh vcpupin $COMPETITOR_VM 4 61
-virsh vcpupin $COMPETITOR_VM 5 62
-virsh vcpupin $COMPETITOR_VM 6 63
-virsh vcpupin $COMPETITOR_VM 7 64
-virsh vcpupin $COMPETITOR_VM 8 141
-virsh vcpupin $COMPETITOR_VM 9 142
-virsh vcpupin $COMPETITOR_VM 10 143
-virsh vcpupin $COMPETITOR_VM 11 144
-virsh vcpupin $COMPETITOR_VM 12 121
-virsh vcpupin $COMPETITOR_VM 13 122
-virsh vcpupin $COMPETITOR_VM 14 123
-virsh vcpupin $COMPETITOR_VM 15 124
+virsh vcpupin $COMPETITOR_VM 0 0
+virsh vcpupin $COMPETITOR_VM 1 0
+virsh vcpupin $COMPETITOR_VM 2 1
+virsh vcpupin $COMPETITOR_VM 3 80
+virsh vcpupin $COMPETITOR_VM 4 80
+virsh vcpupin $COMPETITOR_VM 5 2
+virsh vcpupin $COMPETITOR_VM 6 81
+virsh vcpupin $COMPETITOR_VM 7 3
+virsh vcpupin $COMPETITOR_VM 8 83
+virsh vcpupin $COMPETITOR_VM 9 83
+virsh vcpupin $COMPETITOR_VM 10 4
+virsh vcpupin $COMPETITOR_VM 11 84
+virsh vcpupin $COMPETITOR_VM 12 5
+virsh vcpupin $COMPETITOR_VM 13 85
+virsh vcpupin $COMPETITOR_VM 14 6
+virsh vcpupin $COMPETITOR_VM 15 86
 ssh -T ubuntu@e-vm1 "sudo killall sysbench";
 ssh -T ubuntu@e-vm1 "sudo killall a.out";
 ssh -T ubuntu@e-vm2 "sudo killall sysbench";
