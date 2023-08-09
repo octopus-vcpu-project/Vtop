b8cd860a75c2680b8b19243b63edede276d1a086
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-08-09 15:18:49 -0400

    Update vtop_ult.cpp

--- vtop_ult.cpp
+++ vtop_ult.cpp
@@ -292,7 +292,7 @@ int measure_latency_pair(int i, int j)
 	int max_loops = SAMPLE_US;
 	if(first_measurement){
 		amount_of_times = -6;
-		max_loops = SAMPLE_US * 30;
+		max_loops = SAMPLE_US * 100;
 		first_measurement = false;
 	}
 	
