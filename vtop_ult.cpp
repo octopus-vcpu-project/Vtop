3a531c50ffd7ef00e76792f33668bab4134344ef
dnas-it-team <86024612+dnas-it-team@users.noreply.github.com>
2023-08-09 15:49:59 -0400

    Update vtop_ult.cpp

--- vtop_ult.cpp
+++ vtop_ult.cpp
@@ -599,7 +599,7 @@ bool verify_topology(void){
 	first_measurement = true;
 	for(int i=0;i<nr_numa_groups;i++){
         	for(int j=i+1;j<nr_numa_groups;j++){
-			int latency = measure_latency_pair(numas_to_cpu[i],numas_to_cpu[i+1]);
+			int latency = measure_latency_pair(numas_to_cpu[i],numas_to_cpu[j]);
                 	if(get_latency_class(latency) != 4){
                         	return false;
                 	}
